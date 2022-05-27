acf8e8bc7dccd90f21fab1a25ecdf4c8ce8f09bb
Dipankar Saikia <er.dipankarsaikia@gmail.com>
2022-05-27 23:13:33 +0530

    seeding and custom database seeding learning

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,28 +2,10 @@
 
 namespace App\Models;
 
-class Listing {
-    public static function all() {
-        return [[
-            'id' => 1,
-            'title' => 'Listing One',
-            'description' => 'Finite Automata(FA) is the simplest machine to recognize patterns. The finite automata or finite state machine is an abstract machine that has five elements or tuples. It has a set of states and rules for moving from one state to another but it depends upon the applied input symbol. Basically, it is an abstract model of a digital computer. The following figure shows some essential features of general automation.',
-        ],
-        [
-            'id' => 2,
-            'title' => 'Listing Two',
-            'description' => 'Finite Automata(FA) is the simplest machine to recognize patterns. The finite automata or finite state machine is an abstract machine that has five elements or tuples. It has a set of states and rules for moving from one state to another but it depends upon the applied input symbol. Basically, it is an abstract model of a digital computer. The following figure shows some essential features of general automation.',
-        ]];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id) {
-        $listings = self::all();
-
-        foreach($listings as $listing) {
-            if($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
-
+class Listing extends Model
+{
+    use HasFactory;
 }
